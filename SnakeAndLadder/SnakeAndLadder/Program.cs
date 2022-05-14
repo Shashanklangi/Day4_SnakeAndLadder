94e41f19917d1ffc8c21db0c967b2b8a00d6b13d
Shashank <langishashank@gmail.com>
2022-05-14 20:36:51 +0530

    [Shashank]:Initial position of the player

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SnakeAndLadder
+{
+    public class SnakeAndLadderProgram
+    {
+        public void SnakeAndLadder()
+        {
+
+        }
+        public static void Main(string[] args)
+        {
+            Program program = new Program();    
+            program.SnakeAndLadder();   
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Program
+    {
+        public int Starting_Position = 0;
+        public void SnakeAndLadder()
+        {
+
+            Console.WriteLine(" *****Welcome to the Snake Ans Ladder Game***** ");
+            Console.WriteLine(" Your Initial Position is: " + Starting_Position);
+
+
+        }
+    }
+  
+}              
