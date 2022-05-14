4e50d6a85c99e1cfab0796726535939eda3b7d25
Shashank <langishashank@gmail.com>
2022-05-14 21:25:50 +0530

    [Shashank]: Added Repeating till the player reaches the wining position

--- SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
@@ -12,30 +12,38 @@ namespace SnakeAndLadder
         const int NoPlay = 0, Ladder = 1, Snake = 2;
         public void SnakeAndLadder()
         {
-
-            Random diceroll = new Random(); 
-            int dice =diceroll.Next(1, 7);
-            Console.WriteLine(" Player roll the dice and the number is :" + dice);
-
-            Random check = new Random();
-            int Option = check.Next(0, 3);  
-
-            switch (Option)
+            while (PlayerPosition < 100)
             {
-                case NoPlay:
-                    Console.WriteLine("No Play");
-                    break;
-                case Ladder:
-                    PlayerPosition += dice;
-                    Console.WriteLine("You reached a Ladder : " + PlayerPosition);
-                    break;
-                case Snake:
-                    PlayerPosition -= dice;
-                    Console.WriteLine("You got eaten by a Snake : " + PlayerPosition);
-                    break;
-                default:
-                    Console.WriteLine("Enter a valid input.");
-                    break;
+                Random diceroll = new Random();
+                int dice = diceroll.Next(1, 7);
+                Console.WriteLine(" Player roll the dice and the number is :" + dice);
+
+                Random check = new Random();
+                int Option = check.Next(0, 3);
+
+                switch (Option)
+                {
+                    case NoPlay:
+                        Console.WriteLine("No Play");
+                        break;
+                    case Ladder:
+                        PlayerPosition += dice;
+                        Console.WriteLine("You reached a Ladder : " + PlayerPosition);
+                        break;
+                    case Snake:
+                        PlayerPosition -= dice;
+                        Console.WriteLine("You got eaten by a Snake : " + PlayerPosition);
+                        break;
+                    default:
+                        Console.WriteLine("Enter a valid input.");
+                        break;
+
+                }
+                if(PlayerPosition < 0)
+                {
+                    PlayerPosition = 0;
+                }
+
 
             }
         }
